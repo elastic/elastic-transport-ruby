ffa06119adae00d16688c3e748b2022c8648200e
Fernando Briano <fernando@picandocodigo.net>
2022-09-12 13:58:12 +0100

    [DOCS] Updates source code docs in Response object

--- lib/elastic/transport/transport/response.rb
+++ lib/elastic/transport/transport/response.rb
@@ -19,14 +19,15 @@ module Elastic
   module Transport
     module Transport
       # Wraps the response from Elasticsearch.
-      #
+      # It provides `body`, `status` and `headers` methods, but you can treat is as a hash and
+      # access the keys directly.
       class Response
         attr_reader :status, :body, :headers
 
         # @param status  [Integer] Response status code
         # @param body    [String]  Response body
         # @param headers [Hash]    Response headers
-        def initialize(status, body, headers={})
+        def initialize(status, body, headers = {})
           @status, @body, @headers = status, body, headers
           @body = body.force_encoding('UTF-8') if body.respond_to?(:force_encoding)
         end
