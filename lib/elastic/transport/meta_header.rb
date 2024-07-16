37b310bb62bd67e3572e5143eb629141783502a7
Fernando Briano <fernando@picandocodigo.net>
2024-07-16 16:49:52 +0100

    Updates meta header constants for backwards compatibility in Serverless

--- lib/elastic/transport/meta_header.rb
+++ lib/elastic/transport/meta_header.rb
@@ -42,7 +42,7 @@ module Elastic
         if enterprise_search?
           Elastic::ENTERPRISE_SERVICE_VERSION
         elsif serverless?
-          Elastic::ES_SERVERLESS_SERVICE_VERSION
+          Elastic::ES_SERVERLESS_SERVICE_VERSION || Elastic::ELASTICSEARCH_SERVICE_VERSION
         elsif elasticsearch?
           Elastic::ELASTICSEARCH_SERVICE_VERSION
         elsif defined?(Elasticsearch::VERSION)
