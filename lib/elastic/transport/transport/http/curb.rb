75ef06b6cab4b3c09a219766e94d699cffd74110
Fernando Briano <fernando@picandocodigo.net>
2026-01-09 14:56:58 +0000

    Backports changes to specs/dependencies from main
    Refactors curb implementation. Updates Curb and Manticore unit tests.

--- lib/elastic/transport/transport/http/curb.rb
+++ lib/elastic/transport/transport/http/curb.rb
@@ -43,15 +43,7 @@ module Elastic
               when 'GET', 'POST', 'PUT', 'DELETE'
                 connection.connection.set :nobody, false
                 connection.connection.put_data = body if body
-
-                if headers
-                  if connection.connection.headers
-                    connection.connection.headers.merge!(headers)
-                  else
-                    connection.connection.headers = headers
-                  end
-                end
-
+                parse_headers!(headers, connection)
               else raise ArgumentError, "Unsupported HTTP method: #{method}"
               end
 
@@ -66,6 +58,18 @@ module Elastic
                 decompress_response(connection.connection.body_str),
                 response_headers
               )
+            end
+          end
+
+          # Merges headers already present in the connection and the ones passed in to perform_request
+          #
+          def parse_headers!(headers, connection)
+            return unless headers
+
+            if connection.connection.headers
+              connection.connection.headers.merge!(headers)
+            else
+              connection.connection.headers = headers
             end
           end
 
